bd322ac71d45613fd384894d1a16ed4c328f0312
Tiago Freitas Pereira <tiagofrepereira@gmail.com>
2019-11-03 09:23:59 +0100

    Updated gitbar stuff

--- setup-my-tmux.sh
+++ setup-my-tmux.sh
@@ -3,4 +3,4 @@ cp .tmux-gitbar.cong ~
 
 # git bar shit
 git clone https://github.com/arl/tmux-gitbar.git ~/.tmux-gitbar
-source-file "$HOME/.tmux-gitbar/tmux-gitbar.tmux"
+#source-file "$HOME/.tmux-gitbar/tmux-gitbar.tmux"
